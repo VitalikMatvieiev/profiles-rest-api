b72933d18d7de1b8ab6cfb506c4931fac3f080c0
Sviatoslav Kostyshyn <kossvat1999@gmail.com>
2023-01-17 23:03:46 -0500

    Configured Vagrant and setup hello world script

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["racc",["~> 1.4"]],["nokogiri",[">= 0"]],["vagrant-parallels",["= 2.2.6"]],["vagrant-qemu",["= 0.3.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.31.0"]]],"checksum":"0b1390f00e75f7d5251c5518d0bd27617c6851a4c3e2e7dc479c95f3934c7658","vagrant_version":"2.3.4"}
